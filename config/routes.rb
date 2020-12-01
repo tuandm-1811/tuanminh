2ca7f5ec7e6e23c222d2351960da8f8a7ea1ee01
tuandm-1811 <dominhtuan18112@gmail.com>
2020-12-01 14:55:51 +0700

    project2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+    as :user do 
+      get "signin" => "devise/sessions#new"
+      post "signin" => "devise/sessions#create"
+      delete "signout" => "devise/sessions#destroy"
+    root "static_pages#index"
+    devise_for :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
